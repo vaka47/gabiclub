741e0621df07f920b77ee0151701307b0566d625
Ivan Safonov <safonov47@gmail.com>
2026-05-18 14:05:28 +0300

    Use permanent root redirect to trainings

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/trainings");
+  permanentRedirect("/trainings");
 }
-
